6dbcc63f082fbccdb96d4366d57689046791b615
Qiao Liang <qiaoliang@Qiaos-MacBook-Pro.local>
2017-02-22 23:19:03 -0600

    Restored the right code of LC139

--- LC139.py
+++ LC139.py
@@ -6,20 +6,33 @@ class Solution(object):
     def wordBreak(self, s, wordDict):
         """
         :type s: str
-        :type wordDict: Set[str]
+        :type wordDict: List[str]
         :rtype: bool
         """
-        dp = [False] * (len(s) + 1) # dp[i] means s[:i+1] can be segmented into words in the wordDicts 
-        dp[0] = True
-        for i in range(len(s)):
-            for j in range(i, len(s)):
-                print("*" * 30)
-                print("i: ", i)
-                print("j: ", j)
-                if dp[i] and s[i: j+1] in wordDict:
-                    dp[j+1] = True
-                    print(s[i: j + 1])
-                    print(dp)
-                print("*" * 30)
-                    
-        return dp[-1]
+        self.words = wordDict
+        return self.find(s, [], "")
+        
+    def find(self, s, wrong_words, result):
+        if len(s) == 0:
+            print("Below is the result:")
+            print(result)
+            return True
+        elif s in wrong_words:
+            return False
+        else:
+            i = 0
+            temp = ''
+            
+            while i < len(s):
+                temp += s[i]
+                
+                if temp in self.words:
+                    if self.find(s[i + 1:], wrong_words, result + temp + " "):
+                        return True
+                    else:
+                        i += 1
+                else:
+                    i += 1
+        
+        wrong_words.append(temp)
+        return False
