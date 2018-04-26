86672ff650eb8d71ea3abd62cd8e260d70a7d2fd
Qiao Liang <qiaoliang@Qiaos-MacBook-Pro.local>
2018-04-25 23:22:57 -0500

    Rewrote the LC214 in KMP algorithm to avoid the TLE problem

--- LC214.py
+++ LC214.py
@@ -8,24 +8,41 @@ class Solution(object):
             return ""
 
         len_s = len(s)
+        lmt_s = len_s - 1
+        curr = 1
+        cnt = 0
+        fail = [0] * len_s
+        rev_s = s[::-1]
 
-        for idx in range(len_s - 1, 0, -1):
-            left = 0
-            right = idx
-            while left <= right:
-                if s[left] == s[right]:
-                    left += 1
-                    right -= 1
-                else:
+        while curr < len_s:
+            if s[curr] == s[cnt]:
+                cnt += 1
+                fail[curr] = cnt
+                curr += 1
+            elif cnt > 0:
+                cnt = fail[cnt - 1]
+            else:
+                curr += 1
+
+        curr = curr_rev = 0
+        temp = 0
+
+        while curr < len_s:
+            if s[curr] == rev_s[curr_rev]:
+                if curr_rev == lmt_s:
+                    temp = lmt_s - curr
                     break
-            
-            if right < left:
-                if idx == len_s - 1:
-                    return s
-                else:
-                    return s[idx + 1:][::-1][:] + s
-        
-        return s[1:][::-1] + s
+
+                curr += 1
+                curr_rev += 1
+            elif curr > 0:
+                curr = fail[curr - 1]
+            else:
+                curr_rev += 1
+
+        return rev_s[:temp] + s
+
 
 sol = Solution()
-print(sol.shortestPalindrome("aba"))
+print(sol.shortestPalindrome("abb"))
+# print(sol.shortestPalindrome("aaacecaa"))
